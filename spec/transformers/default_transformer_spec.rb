67bad3971263a25b6c5cfd5aaea0be1cd740ade1
bosunogunlana <tunbosunogunlana1@gmail.com>
2022-07-19 16:33:38 +0100

    refactor: rename processed_ to transformed_

--- spec/transformers/default_transformer_spec.rb
+++ spec/transformers/default_transformer_spec.rb
@@ -25,22 +25,22 @@ describe Transformers::DefaultTransformer do
   end
 
   describe '.call' do
-    it 'return processed log' do
-      processed_log = described_class.call(log_file)
-      expect(processed_log['/contact'][:visit_counts]).to eq(3)
-      expect(processed_log['/contact'][:visitors_ip]).to eq(Set['184.123.665.067', '543.910.244.929'])
+    it 'return transformed log' do
+      transformed_log = described_class.call(log_file)
+      expect(transformed_log['/contact'][:visit_counts]).to eq(3)
+      expect(transformed_log['/contact'][:visitors_ip]).to eq(Set['184.123.665.067', '543.910.244.929'])
     end
 
-    it 'sort processed logs by all page visits in descending order' do
-      processed_log = described_class.call(log_file, type: :all)
-      expect(processed_log.to_a).to eq(all_processed_log)
-      expect(processed_log.first.first).to eq('/help_page/1')
+    it 'sort transformed logs by all page visits in descending order' do
+      transformed_log = described_class.call(log_file, type: :all)
+      expect(transformed_log.to_a).to eq(all_processed_log)
+      expect(transformed_log.first.first).to eq('/help_page/1')
     end
 
-    it 'sort processed logs by unique page visits in descending order' do
-      processed_log = described_class.call(log_file, type: :unique)
-      expect(processed_log.to_a).to eq(unique_processed_log)
-      expect(processed_log.first.first).to eq('/help_page/1')
+    it 'sort transformed logs by unique page visits in descending order' do
+      transformed_log = described_class.call(log_file, type: :unique)
+      expect(transformed_log.to_a).to eq(unique_processed_log)
+      expect(transformed_log.first.first).to eq('/help_page/1')
     end
   end
 end
